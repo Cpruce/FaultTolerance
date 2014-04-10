2862b745846a90daa2ef1ae586401cad8b4efea8
Cory Pruce <cwpruce@gmail.com>
2014-04-09 21:35:21 -0700

    update

--- tests/automated_testing/simple_test.sh
+++ tests/automated_testing/simple_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 erlc storage_process.erl
-erlc key_value_node_temp.erl
-erl -noshell -run key_value_node_temp main 3 node1
+erlc key_value_node.erl
+erl -noshell -run key_value_node main 3 node1
 # erlc key_value_node.erl && erl -noshell -run key_value_node main 10 node1
