56fcaa8fbaebae25e49c7d75b3252fd0c2fd2eee
Tum Chaturapruek <tummykung@gmail.com>
2014-04-10 12:24:16 -0700

    Update the working version.

--- simple_test_working.sh
+++ simple_test_working.sh
@@ -5,4 +5,4 @@ erlc non_storage_process.erl
 # erlc advertise_id.erl
 erlc key_value_node_working.erl
 erl -noshell -run key_value_node_working main 3 node1
-# erlc key_value_node.erl && erl -noshell -run key_value_node main 3 node2 node1@Js-MacBook-Pro-8
+# erlc key_value_node_working.erl && erl -noshell -run key_value_node_working main 3 node2 node1@Js-MacBook-Pro-8
